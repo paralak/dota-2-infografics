9c2468f7fe9a43b9d564ddee4464aa12f743d8ef
paralak <84019442+paralak@users.noreply.github.com>
2022-06-06 18:50:05 +0300

    Revert "0.2-pre-1"

--- info.js
+++ info.js
@@ -30,6 +30,7 @@ const games = {
     ['sand-king', 'mirana', 'invoker', 'chen', 'wraith-king'],
     ['pangolier', 'witch-doctor', 'outworld-destroyer', 'phantom-lancer', 'ancient-apparition']
   ],
+
   betboom: [
     ['storm-spirit', 'visage', 'underlord', 'ancient-apparition', 'chen'],
     ['mars', 'visage', 'storm-spirit', 'shadow-demon', 'jakiro'],
@@ -52,6 +53,7 @@ const games = {
     ['pangolier', 'death-prophet', 'techies', 'oracle', 'huskar'],
     ['pugna', 'storm-spirit', 'grimstroke', 'spirit-breaker', 'visage']
   ],
+
   eg: [
     ['storm-spirit', 'medusa', 'jakiro', 'mars', 'io'],
     ['storm-spirit', 'medusa', 'death-prophet', 'tidehunter', 'grimstroke'],
@@ -73,8 +75,43 @@ const games = {
     ['enchantress', 'tiny', 'tidehunter', 'rubick', 'void-spirit'],
     ['storm-spirit', 'tusk', 'mars', 'disruptor', 'morphling']
   ]
+
 }
 
+
+
+
+
+let games1 = games[prompt('Выберите команду:')];
+let coef = prompt('Количество игр с героем, чтобы он отобразился на схеме');
+
+
+
+
+const heroes = {};
+
+games1.forEach((item,i) => {
+  item.forEach((her,i) => {
+    if (!(her in heroes)) {
+      heroes[her] = {}}
+    item.forEach((her2,i) => {
+      if (!(her2 in heroes[her])) {
+        heroes[her][her2] = 0
+      }
+      heroes[her][her2]+=1
+    })
+  })
+});
+
+const heroes2 = {};
+
+for (let i in heroes) {
+  if (heroes[i][i]>=coef) {
+    heroes2[i] = heroes[i];
+  };
+};
+
+console.log("done 1")
 
 
 //mas = document.getElementsByClassName("picks-inline")
